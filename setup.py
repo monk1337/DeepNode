f6f527ff03acc198356741860d6de47884810b8e
monk1337 <3x3paul@gmail.com>
2019-12-27 23:04:06 +0530

    check

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+
+setup(
+    name='DeepNode',
+    version='0.1.0',
+    description='Framework for Graph Neural Networks',
+    author='Monk1337',
+    author_email='checkmate',
+    url='you_know_who_am.ai'
+)
